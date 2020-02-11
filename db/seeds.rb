f8d182fbffc0dce0e1305910b05f6f7ade9c44fc
Mathieu Guillermit <mguillermit@gmail.com>
2020-02-11 16:29:34 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  puts Article.create(name: Faker::GreekPhilosophers.name, content: Faker::GreekPhilosophers.quote)
+end
